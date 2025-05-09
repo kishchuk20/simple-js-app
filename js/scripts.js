447a55d62c0202635a0ae908010ce634f43361ef
kishchuk20 <issukk1237@gmail.com>
2025-05-09 14:23:11 -0400

    Big pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 let pokemonList = [
-    {
-      name: "Bulbasaur",
-      height: 7,
-      types: ['grass', 'poison']
-    },
-    {
-      name: "Nidoran",
-      height: 5,
-      types: ['hustle', 'poison']
-    },
-    {
-      name: "Pikachu",
-      height: 4,
-      types: ['static', 'lightningrod']
-    }
+    { name: "Bulbasaur", height: 0.7 },
+    { name: "Charizard", height: 1.7 },
+    { name: "Pikachu", height: 0.4 }
   ];
+
+// Write Pokemon's name and height
+  for (let i=0; i<pokemonList.length; i++){
+    let pokemon = pokemonList[i];
+    let output = pokemon.name + " (height: " + pokemon.height + ")";
+
+    // finds a big Pokemon
+    if (pokemonList[i].height >1){
+      output += " - Wow,that's big!";
+      console.log(pokemonList[i].name.height + " Wow,that's big!")
+    }
+    document.write("<p>" + output + "</p>");
+  }
